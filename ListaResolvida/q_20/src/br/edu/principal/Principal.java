8fcfb12271b6a2dbd309ba52340d59af208bd3bd
Júlia Maria <125198189+Julia-maria56@users.noreply.github.com>
2023-04-22 20:19:27 -0300

    Update Principal.java

--- ListaResolvida/q_20/src/br/edu/principal/Principal.java
+++ ListaResolvida/q_20/src/br/edu/principal/Principal.java
@@ -5,7 +5,10 @@ public class Principal {
         int fim = 65;
         int incremento = 1;
 
+        System.out.println("************************************************************************************");
         System.out.println("Tabela de graus Celsius e graus Fahrenheit, cujos graus variam de 50 a 65 de 1 cm 1.");
+        System.out.println("************************************************************************************\n");
+
 
         for (int c = inicio; c <= fim; c += incremento) {
             double f = 1.8 * c + 32;
